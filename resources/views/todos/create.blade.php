d24a0c4f1bd0e83f22db60ef277ce9f9769027fb
stalg94 <stalg28@gmail.com>
2021-11-20 00:54:38 +0200

    Show All Todo

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,25 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>To Do</title>
-</head>
-<body>
-<div class="text-center pt-10">
-    <h1 class="text2x1">What next you need to do?</h1>
-    <x-alert />
-    <form method="post" action="/todos/create" class="py-5 ">
-        @csrf
-        <input type="text" name="title" class="py-2 px-2 border rounded">
-{{--        <input type="submit" value="Create" >--}}
-        <button style="cursor: pointer" type="submit" value="Create" class=" py-2 border rounded">Create</button>
-    </form>
-</div>
+@extends('todos.layout')
 
+@section('content')
+<h1 class="text2x1">What next you need to do?</h1>
+<x-alert />
+<form method="post" action="/todos/create" class="py-5 ">
+    @csrf
+    <input type="text" name="title" class="py-2 px-2 border rounded">
+    {{--        <input type="submit" value="Create" >--}}
+    <button style="cursor: pointer" type="submit" value="Create" class=" py-2 border rounded">Create</button>
+</form>
 
-</body>
-</html>
+@endsection
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,15 @@
-All todos
+@extends('todos.layout')
+
+@section('content')
+<h1 class="text2x1">All Your To Dos</h1>
+<ul>
+    @foreach( $todos as $todo)
+        <li>
+            {{$todo->title}}
+        </li>
+    @endforeach
+</ul>
+
+@endsection
+
+
